83e65532f29b86638af6c1ca6c65f10cbe3da736
Zerothl <stedwar10@hotmail.com>
2024-11-07 12:22:28 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-// Agrega el servicio PokemonService
 builder.Services.AddScoped<PokemonService>();
 
 await builder.Build().RunAsync();
